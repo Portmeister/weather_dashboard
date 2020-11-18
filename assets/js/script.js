59676fbcd46cf7be2763cdfadcd1493fa9624d89
Portmeister <portmeister@gmail.com>
2020-11-17 23:33:07 -0600

    add ajax calls for weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var apiKey = "69fc1986ec912e165f8bfed54768b04b";
-var queryURL1 = "http://api.openweathermap.org/data/2.5/weather";
-var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi";
-var queryURL3 = "http://api.openweathermap.org/data/2.5/forecast";
+var apiKey = "&APPID=69fc1986ec912e165f8bfed54768b04b";
+var queryURL1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=";
+var queryURL3 = "http://api.openweathermap.org/data/2.5/forecast?q=";
 var formEl = $('.form-inline');
 var nameInputEl = $('.form-control');
 var cityListEl = $('#city-list');
@@ -25,8 +25,39 @@ var handleFormSubmit = function (event) {
 
   printCity(nameInput);
 
+  var weatherURL = queryURL1 + nameInput + apiKey;
+
+  $.ajax({
+    url: weatherURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    var uviParam = response.coord.lat + "&lon=" + response.coord.lon
+    var uviURL = queryURL2 + uviParam + apiKey;
+
+    $.ajax({
+        url: uviURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+
+        var forecastURL = queryURL3 + nameInput + apiKey;
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+          });
+
+      });
+
+  });
+
   // resets form
   nameInputEl.val('');
+  
 };
 
 formEl.on('submit', handleFormSubmit);

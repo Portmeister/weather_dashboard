21a0f8f28c3eb0f49d012ba913d56b3b00827834
Portmeister <portmeister@gmail.com>
2020-11-18 11:17:58 -0600

    add js code for displaying ajax response of city, weather, uvi and moment.js plus span and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ var queryURL3 = "http://api.openweathermap.org/data/2.5/forecast?q=";
 var formEl = $('.form-inline');
 var nameInputEl = $('.form-control');
 var cityListEl = $('#city-list');
+var cityDisplay = $('#current');
+var cityTitle = $("<h2>");
+var cityTemp = $("<p>");
+var cityHumid = $("<p>");
+var cityWind = $("<p>");
+var cityUVI = $("<p>");
 
 var printCity = function (name) {
   var listEl = $('<li>');
@@ -17,6 +23,7 @@ var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var nameInput = nameInputEl.val();
+  localStorage.setItem("city", nameInput);
  
   if (!nameInput) {
     console.log('You need to fill out the form!');
@@ -32,6 +39,10 @@ var handleFormSubmit = function (event) {
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    cityTitle.text(response.name + " " + moment().format("MMM Do YYYY")).appendTo(cityDisplay);
+    cityTemp.text("Temperature: " + ((response.main.temp - 273.15) * 9/5 + 32).toFixed(2) + "°F").appendTo(cityDisplay);
+    cityHumid.text("Humidity: " + response.main.humidity).appendTo(cityDisplay);
+    cityWind.text("Wind Speed: " + response.wind.speed).appendTo(cityDisplay);
 
     var uviParam = response.coord.lat + "&lon=" + response.coord.lon
     var uviURL = queryURL2 + uviParam + apiKey;
@@ -41,6 +52,13 @@ var handleFormSubmit = function (event) {
         method: "GET"
       }).then(function(response) {
         console.log(response);
+        cityUVI.text("UV Index: ");
+        cityUVI.append(
+            "<span class='primary'>" +
+          response.value +
+          "</span>"
+        ).appendTo(cityDisplay);
+
 
         var forecastURL = queryURL3 + nameInput + apiKey;
 
